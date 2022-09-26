052e72d8eb7378c2e76592d9b99e32b346ac9fb9
Ihsan <mi598342@gmail.com>
2022-09-26 14:16:54 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Put your code here!!
+addEventListener('fetch', event => {
+  const { request } = event;
+  const { url } = request;
+  if (url.includes('submit')) {
+    return event.respondWith(new Response(htmlForm, {headers:{"Content-Type":"text/html"}}));
+  }
+  if (request.method === 'POST') {
+    return event.respondWith(handleRequest(request));
+  } else if (request.method === 'GET') {
+    return event.respondWith(new Response(`The request was a GET`));
+  }
+});
