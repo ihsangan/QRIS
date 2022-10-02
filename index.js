4b0b971b70ecc5e3789d36441f5bf67d4ce8a8c9
Ihsan <mi598342@gmail.com>
2022-10-03 06:00:49 +0800

    Try to use GET #2

--- index.js
+++ index.js
@@ -31,9 +31,5 @@ addEventListener('fetch', event => {
   if (url.includes('/submit')) {
     return event.respondWith(new Response(htmlForm, {headers:{"Content-Type":"text/html"}}));
   }
-  if (request.method === 'POST') {
     return event.respondWith(handleRequest(request));
-  } else if (request.method === 'GET') {
-    return event.respondWith(new Response('https://github.com/ihsangan/qris'));
-  }
 });
