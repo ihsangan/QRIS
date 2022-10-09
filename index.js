6ac37922caaf7c0acdcd8de7c2de51d773fbe23e
Ihsan <mi598342@gmail.com>
2022-10-09 14:27:12 +0800

    update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function handleRequest(request) {
   let name = getMerchName(d);
   let price = `Rp${p.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.")},00`
   if (o === 'html') {
-    let content = `<!doctype html><html><head><meta name="viewport" content="width=device-width,initial-scale=1"/><title>QRIS</title></head><body><img src="https://cdn.jsdelivr.net/gh/ihsangan/files/qris.svg" alt="QRIS logo" width="220" style="margin:27px 0"/><img src="https://cdn.jsdelivr.net/gh/ihsangan/files/gpn.svg" alt="GPN logo" width="50" style="float:right"/><br><center><a href="https://qr.isan.eu.org/v1/create-qr-code/?size=280x280&ecc=Q&qzone=7&margin=0&&format=png&data=${data}" download="QRIS-${name}-${price}"><img src="https://qr.isan.eu.org/v1/create-qr-code/?size=350x350&ecc=Q&qzone=0&margin=0&&format=svg&data=${data}" alt="QRIS data" height="350"/></a><br><h3>${price} ke ${name}</body></html>`;
+    let content = `<!doctype html><html><head><meta name="viewport" content="width=device-width,initial-scale=1"/><title>QRIS</title></head><body><img src="https://cdn.jsdelivr.net/gh/ihsangan/files/qris.svg" alt="QRIS logo" width="220" style="margin:27px 0"/><img src="https://cdn.jsdelivr.net/gh/ihsangan/files/gpn.svg" alt="GPN logo" width="50" style="float:right"/><br><center><img src="https://qr.isan.eu.org/v1/create-qr-code/?size=350x350&ecc=Q&qzone=0&margin=0&&format=svg&data=${data}" alt="QRIS data" height="350"/><br><h3>${price} ke ${name}</body></html>`;
     return new Response(content, {
       headers: { "Content-Type": "text/html" }
     })
