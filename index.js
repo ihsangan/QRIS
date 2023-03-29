3c1a1b2e47c546fa0072f6a61fce04aa80e32014
Ihsan <mi598342@gmail.com>
2023-03-29 11:17:37 +0800

    update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function crc16($) {
 };
 let htmlForm = `<!DOCTYPE html><html><head><meta content="width=device-width,initial-scale=1" name="viewport"><title>Dynamic QRIS Generator</title><body><form action="/" method="POST" autocomplete="on"><textarea name="data" rows="4" cols="30" placeholder="QRIS data" required></textarea><br><input type="number" name="price" placeholder="Price" required/><select name="output"><option selected value="html">HTML</option><option value="json">JSON</option><option value="raw">RAW</option></select><br><input type="submit" value="submit"></form></script></body></html>`;
 function generateQRIS(d, p, dyn) {
-  //if (dyn === true) {
+  if (dyn === true) {
     let data = d.slice(0, -4).replace('11', '12').replace('3360', `3360540${p.length + 3}${p}.00`)
- // }
+  }
   //else {
     //let data = d.slice(0, -4).replace('3360', `3360540${p.length + 3}${p}.00`)
  // }
