380414654e5ff9cd43eb180e1426409e8f80b5a5
ihsangan <me@isan.eu.org>
2024-03-06 09:23:07 +0800

    re auth

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function crc16($) {
   }
   return (0 ^ A) & 65535
 };
-let htmlForm = `<!DOCTYPE html><html><head><meta content="width=device-width,initial-scale=1" name="viewport"><title>Dynamic QRIS Generator</title><body><form action="/" method="POST" autocomplete="on"><textarea name="data" rows="4" cols="30" placeholder="QRIS data" required></textarea><br><input type="number" name="price" placeholder="Price" required/><select name="output"><option selected value="html">HTML</option><option value="json">JSON</option><option value="raw">RAW</option></select><br><input type="submit" value="submit"></form></script></body></html>`;
+let htmlForm = `<!DOCTYPE html><html><head><meta content="width=device-width,initial-scale=1" name="viewport"><title>Dynamic QRIS Generator</title><body><form action="https://api.isan.eu.org/qris/" method="POST" autocomplete="on"><textarea name="data" rows="4" cols="30" placeholder="QRIS data" required></textarea><br><input type="number" name="price" placeholder="Price" required/><select name="output"><option selected value="html">HTML</option><option value="json">JSON</option><option value="raw">RAW</option></select><br><input type="submit" value="submit"></form></script></body></html>`;
 function generateQRIS(d, p) {
   let data = d.slice(0, -4).replace('11', '12').replace('3360', `3360540${p.length + 3}${p}.00`)
   let c = crc16(data).toString(16).toUpperCase()
